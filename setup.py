399659be2a76bdd065611c3cefbd399e5cecf0be
ccz <cczchina@sjtu.edu.cn>
2026-05-09 15:18:04 +0800

    add mmpretrain

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import sys
 import warnings
 from setuptools import find_packages, setup
 
+
 import torch
 from torch.utils.cpp_extension import (BuildExtension, CppExtension,
                                        CUDAExtension)
